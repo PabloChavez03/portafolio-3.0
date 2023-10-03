ad7e0542ce6c276a85fc818d31b59d83a4b12df9
PabloChavez03 <pablooscarchavez@gmail.com>
2023-10-02 21:32:48 -0300

    add icons, images & seo

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 export const SITE = {
   author: 'chavezpablo_',
   githubName: 'PabloChavez03',
-  description: 'Portfolio web'
+  description: 'Portfolio web',
+  origin: 'http://localhost:4321/',
+  canonical: 'http://localhost:4321/'
 }
---
+++ src/info/projects.ts
@@ -0,0 +1,35 @@
+export const PROJECTS = [{
+  title: 'Webictionary',
+  description: '📖 Diccionario web en inglés',
+  href: 'https://webictionary.netlify.app/',
+  repositoryUrl: 'https://github.com/PabloChavez03/webictionary',
+  techs: ['React', 'Typescript', 'Vite']
+},
+{
+  title: 'Ascend',
+  description: '🎨 Landing page de un emprendimiento.',
+  repositoryUrl: 'https://github.com/PabloChavez03/Ascend',
+  href: 'https://ascend-asesoriaintegral.netlify.app/',
+  techs: ['Astro', 'Typescript', 'Tailwind CSS']
+},
+{
+  title: 'PI Food',
+  description: '🍔 SPA de recetas.',
+  href: 'https://pi-food-main-opal.vercel.app/',
+  repositoryUrl: 'https://github.com/PabloChavez03/PI-Food-main',
+  techs: ['React', 'Redux', 'Node', 'Express', 'PostgreSQL', 'Sequelize']
+},
+{
+  title: 'Clothes 22',
+  description: '👕 E-commerce de ropa.',
+  href: 'https://clothes-22.vercel.app/',
+  repositoryUrl: 'https://github.com/PabloChavez03/ecommerce-pf',
+  techs: ['React', 'Redux', 'Node', 'Express', 'PostgreSQL', 'Sequelize', 'Passport']
+},
+{
+  title: 'Marksite',
+  description: '🔠 App básica para escribir en Markdown.',
+  href: 'https://marksite-project.netlify.app/',
+  repositoryUrl: 'https://github.com/PabloChavez03/Marksite',
+  techs: ['React', 'Typescript', 'Tailwind CSS']
+}]
