5b8b44ce109763bc9271e24292479b16ef5696a1
PabloChavez03 <pablooscarchavez@gmail.com>
2023-11-29 09:13:49 -0300

    add new projects & style arrow with not href

--- src/info/projects.ts
+++ src/info/projects.ts
@@ -1,35 +1,57 @@
-export const PROJECTS = [{
-  title: 'Webictionary',
-  description: '📖 Diccionario web en inglés',
-  href: 'https://webictionary.netlify.app/',
-  repositoryUrl: 'https://github.com/PabloChavez03/webictionary',
-  techs: ['React', 'Typescript', 'Vite']
-},
-{
-  title: 'Ascend',
-  description: '🎨 Landing page de un emprendimiento.',
-  repositoryUrl: 'https://github.com/PabloChavez03/Ascend',
-  href: 'https://ascend-asesoriaintegral.netlify.app/',
-  techs: ['Astro', 'Typescript', 'Tailwind CSS']
-},
-{
-  title: 'PI Food',
-  description: '🍔 SPA de recetas.',
-  href: 'https://pi-food-main-opal.vercel.app/',
-  repositoryUrl: 'https://github.com/PabloChavez03/PI-Food-main',
-  techs: ['React', 'Redux', 'Node', 'Express', 'PostgreSQL', 'Sequelize']
-},
-{
-  title: 'Clothes 22',
-  description: '👕 E-commerce de ropa.',
-  href: 'https://clothes-22.vercel.app/',
-  repositoryUrl: 'https://github.com/PabloChavez03/ecommerce-pf',
-  techs: ['React', 'Redux', 'Node', 'Express', 'PostgreSQL', 'Sequelize', 'Passport']
-},
-{
-  title: 'Marksite',
-  description: '🔠 App básica para escribir en Markdown.',
-  href: 'https://marksite-project.netlify.app/',
-  repositoryUrl: 'https://github.com/PabloChavez03/Marksite',
-  techs: ['React', 'Typescript', 'Tailwind CSS']
-}]
+export const PROJECTS = [
+  {
+    title: "Webictionary",
+    description: "📖 Diccionario web en inglés",
+    href: "https://webictionary.netlify.app/",
+    repositoryUrl: "https://github.com/PabloChavez03/webictionary",
+    techs: ["React", "Typescript", "Vite"],
+  },
+  {
+    title: "Ascend",
+    description: "🎨 Landing page de un emprendimiento.",
+    repositoryUrl: "https://github.com/PabloChavez03/Ascend",
+    href: "https://ascend-asesoriaintegral.netlify.app/",
+    techs: ["Astro", "Typescript", "Tailwind CSS"],
+  },
+  {
+    title: "Marksite",
+    description: "🔠 App básica para escribir en Markdown.",
+    href: "https://marksite-project.netlify.app/",
+    repositoryUrl: "https://github.com/PabloChavez03/Marksite",
+    techs: ["React", "Typescript", "Tailwind CSS"],
+  },
+  {
+    title: "Practicando React",
+    description: "⚛️ Repositorio multipaquete con proyectos de React",
+    repositoryUrl: "https://github.com/PabloChavez03/practicando-react",
+    techs: ["React", "Typescript", "Tailwind CSS", "Zustand"],
+  },
+  {
+    title: "Pomodoro",
+    description: "🍎 App sencilla de Pomodoro",
+    repositoryUrl: "https://github.com/PabloChavez03/pomodoro-mobile",
+    techs: ["React Native", "Expo Go"],
+  },
+  {
+    title: "PI Food",
+    description: "🍔 SPA de recetas.",
+    href: "https://pi-food-main-opal.vercel.app/",
+    repositoryUrl: "https://github.com/PabloChavez03/PI-Food-main",
+    techs: ["React", "Redux", "Node", "Express", "PostgreSQL", "Sequelize"],
+  },
+  {
+    title: "Clothes 22",
+    description: "👕 E-commerce de ropa.",
+    href: "https://clothes-22.vercel.app/",
+    repositoryUrl: "https://github.com/PabloChavez03/ecommerce-pf",
+    techs: [
+      "React",
+      "Redux",
+      "Node",
+      "Express",
+      "PostgreSQL",
+      "Sequelize",
+      "Passport",
+    ],
+  },
+];
